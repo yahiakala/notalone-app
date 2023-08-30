607d073985362dacd2cfd5126051a0dc80f173eb
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-08-30 20:45:54 +0000

    Edited MembersComponent.ItemTemplate3 and MembersComponent

--- client_code/MembersComponent/ItemTemplate3/__init__.py
+++ client_code/MembersComponent/ItemTemplate3/__init__.py
@@ -5,7 +5,9 @@ from anvil import *
 class ItemTemplate3(ItemTemplate3Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        print("Client: Getting member data binding ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.init_components(**properties)
+        print("Client: Got member data binding ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.pmt_success = 'Member is in good standing with payments.'
         if self.item['payment_email']:
             pmt_email = self.item['payment_email']
@@ -14,6 +16,7 @@ class ItemTemplate3(ItemTemplate3Template):
         self.pmt_fail = 'The member has not paid or is not using the email(s): ' + self.item['email'] + ', ' + pmt_email
         self.lbl_payment_status.text = self.pmt_success if self.item['payment_enrolled'] else self.pmt_fail
 
+    
     def btn_remove_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.item['roles'] = ['removed']
--- client_code/MembersComponent/__init__.py
+++ client_code/MembersComponent/__init__.py
@@ -7,8 +7,11 @@ from .. import Global
 class MembersComponent(MembersComponentTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        print("Client: Getting members ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.members = Global.users.search(roles=['member'])
+        print("Client: Got members ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.init_components(**properties)
+        print("Client: Got data bindings ", dt.datetime.now().strftime("%H:%M:%S.%f"))
 
         self.rp_members.add_event_handler('x-refresh1', self.update_stuff)
 
