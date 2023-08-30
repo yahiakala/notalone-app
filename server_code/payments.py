69ebdfb540ae075c7aab648cfad4aeeef84b7850
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-08-30 23:08:47 +0000

    Created payments; Edited settings

---
+++ server_code/payments.py
@@ -0,0 +1,29 @@
+import anvil.server
+import anvil.secrets
+
+import requests
+
+
+def get_paypal_auth():
+    client_id = anvil.secrets.get_secret('paypal_client_id')
+    client_secret = anvil.secrets.get_secret('paypal_secret')
+    
+    auth_response = requests.post('https://api.paypal.com/v1/oauth2/token', 
+                                auth=(client_id, client_secret), 
+                                headers={'Accept': 'application/json', 'Accept-Language': 'en_US'}, 
+                                data={'grant_type': 'client_credentials'})
+    
+    auth_response_json = auth_response.json()
+    access_token = auth_response_json['access_token']
+    return access_token
+
+
+def get_subscriptions(subscription_id):
+    access_token = get_paypal_auth()
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+    }
+    subscription_detail_response = requests.get(f'https://api.paypal.com/v1/billing/subscriptions/{subscription_id}', headers=headers)
+    status = subscription_detail_response.json()['status']
+    return status == 'ACTIVE'
