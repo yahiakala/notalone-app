8583fe3c08eb2b2fd31e352c561592f2eec77f4e
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-08-30 16:24:09 +0000

    Edited ApplicantsComponent

--- client_code/ApplicantsComponent/__init__.py
+++ client_code/ApplicantsComponent/__init__.py
@@ -4,14 +4,21 @@ import anvil.server
 
 from .. import Global
 
+import datetime as dt
+
 
 class ApplicantsComponent(ApplicantsComponentTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        print("Client: Getting Applicants ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.applicants = Global.applicants
+        print("Client: Getting applied ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.applied = [i for i in self.applicants if i['roles'] == []]
+        print("Client: Getting pending ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.pending = [i for i in self.applicants if i['roles'] == ['pending']]
+        print("Client: Getting data bindings ", dt.datetime.now().strftime("%H:%M:%S.%f"))
         self.init_components(**properties)
+        print("Client: Done getting data bindings ", dt.datetime.now().strftime("%H:%M:%S.%f"))
 
         print('Pending Applicants: ', [i for i in self.applicants if 'pending' in i['roles']])
         self.rp_applicants.add_event_handler('x-refresh1', self.update_stuff)
