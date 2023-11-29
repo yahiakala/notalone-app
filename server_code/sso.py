31d5784666eea9b24ad0ec878aa02a8e9ebfa15f
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-11-29 23:08:58 +0000

    Edited sso

--- server_code/sso.py
+++ server_code/sso.py
@@ -2,11 +2,11 @@ import anvil.server
 import anvil.users
 
 
-@anvil.server.http_endpoint('/login-sso')
+@anvil.server.http_endpoint('/login-sso', cross_site_session=True)
 def login_sso(**params):
     # params['key']
     user = anvil.users.get_user(allow_remembered=True)
     if user:
         return user
     else:
-        anvil.users.login_with_form(allow_remembered=True)
+        return "user not logged in"
