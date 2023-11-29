76ccf7e71b9815bb844ea2f4ec0f5dc058430a65
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-11-29 23:09:52 +0000

    Edited sso

--- server_code/sso.py
+++ server_code/sso.py
@@ -7,6 +7,6 @@ def login_sso(**params):
     # params['key']
     user = anvil.users.get_user(allow_remembered=True)
     if user:
-        return user
+        return "User logged in"
     else:
         return "user not logged in"
