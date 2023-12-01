cee884ac03ce3d9f7ce641488c86ae5d27e24c61
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-12-01 18:06:04 +0000

    Edited ForumiComponent

--- client_code/ForumiComponent/__init__.py
+++ client_code/ForumiComponent/__init__.py
@@ -1,9 +1,12 @@
 from ._anvil_designer import ForumiComponentTemplate
 from anvil import *
 
+
 class ForumiComponent(ForumiComponentTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        # Any code you write here will run before the form opens.
+    def form_show(self, **event_args):
+        """This method is called when the HTML panel is shown on the screen"""
+        self.call_js('createAndSetIframeSrc', "https://www.wikipedia.com")
