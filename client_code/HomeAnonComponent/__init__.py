a4d161ac0a6b41729f61a446a15038f755c63152
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-12-02 15:43:57 +0000

    Edited settings, HomeForm and HomeAnonComponent

--- client_code/HomeAnonComponent/__init__.py
+++ client_code/HomeAnonComponent/__init__.py
@@ -1,9 +1,9 @@
 from ._anvil_designer import HomeAnonComponentTemplate
 from anvil import *
+import anvil.server
+
 
 class HomeAnonComponent(HomeAnonComponentTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
-        # Any code you write here will run before the form opens.
