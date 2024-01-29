2c029627885cfd44c157572fe150901acff31833
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-29 01:22:36 +0000

    step 1 of client test refactor

---
+++ client_code/Tests/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import TestsTemplate
+from anvil import *
+from .. import test_forms, test_server, test_tasks
+
+
+class Tests(TestsTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        self.ctests.test_modules = [test_forms, test_server, test_tasks]
+        self.ctests.card_roles=['tonal-card', 'elevated-card', 'elevated-card']
+        self.ctests.icon_size=30
+        self.ctests.btn_role='filled-button'
