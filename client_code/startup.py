6b4cf0590fe8bb02650d9476bb925593f0240201
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-06-25 18:03:38 +0000

    Edited startup

--- client_code/startup.py
+++ client_code/startup.py
@@ -2,11 +2,17 @@ from anvil_extras import routing
 from .Router import Router
 from .BlankTemplate import BlankTemplate
 from .Static import Static
+from .Global import Global
+
+
+@routing.redirect(path="app", priority=20, condition=lambda: Global.user is None)
+def redirect_no_user():
+    return "sign"
 
 
 hash, pattern, dict = routing.get_url_components()
 
 # Loads the template form
-routing.set_url_hash(pattern)
+routing.set_url_hash(hash)
 
 routing.launch()
