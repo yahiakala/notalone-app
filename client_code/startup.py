1cee97b82d7b86082073cb187af445785192cd00
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-06-25 18:02:47 +0000

    Created Not Alone 2.png; Edited Signin, startup, Sign and Signup

--- client_code/startup.py
+++ client_code/startup.py
@@ -1,6 +1,7 @@
 from anvil_extras import routing
 from .Router import Router
 from .BlankTemplate import BlankTemplate
+from .Static import Static
 
 
 hash, pattern, dict = routing.get_url_components()
