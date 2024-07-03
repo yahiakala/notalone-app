765d72e9d438c09a85f780d1ca2449887c199bec
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-03 18:21:13 +0000

    Edited startup

--- client_code/startup.py
+++ client_code/startup.py
@@ -4,8 +4,6 @@ from .Router import Router
 from .Static import Static
 from .Global import Global
 from .StaticLaunch import StaticLaunch
-from anvil_squared.utils import print_timestamp
-print_timestamp('Startup')
 
 
 @routing.redirect(path="app", priority=20, condition=lambda: Global.user is None)
@@ -21,6 +19,5 @@ hash, pattern, dict = routing.get_url_components()
 
 # Loads the template form
 routing.set_url_hash(hash)
-print_timestamp('before launch')
+
 routing.launch()
-print_timestamp('end startup')
