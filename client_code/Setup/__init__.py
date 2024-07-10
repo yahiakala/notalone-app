2c33c74865b519e5c8b4a8c674f3ff5080a5787c
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-10 21:22:03 +0000

    Edited settings and Setup

--- client_code/Setup/__init__.py
+++ client_code/Setup/__init__.py
@@ -10,5 +10,11 @@ class Setup(SetupTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        
+
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
         self.tenant = Global.tenant
+        self.tb_name.text = self.tenant['name']
+        self.tb_waiver_link.text = self.tenant['waiver']        
         
