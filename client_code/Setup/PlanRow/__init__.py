cac5c2fe51eb525e90594da358bcb431fb0eedc2
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-11 16:00:20 +0000

    Edited Setup.PlanRow

--- client_code/Setup/PlanRow/__init__.py
+++ client_code/Setup/PlanRow/__init__.py
@@ -14,7 +14,8 @@ class PlanRow(PlanRowTemplate):
 
     def btn_show_roles_click(self, **event_args):
         """This method is called when the button is clicked"""
-        self.plan_role_perm = self.item
+        self.plan_role_perm = {}
+        self.plan_role_perm['roles'] = self.item['roles'].copy()
         self.plan_role_perm['roles'] = [i for i in Global.roles if i['name'] in self.item['roles']]
         new_roles = routing.alert(RoleSelect(item=self.plan_role_perm))
         if new_roles:
