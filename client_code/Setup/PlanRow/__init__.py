e36593109b1a0249bfd72a0d555122f0a0436142
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-11 15:58:54 +0000

    Edited Setup.PlanRow

--- client_code/Setup/PlanRow/__init__.py
+++ client_code/Setup/PlanRow/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import PlanRowTemplate
 from anvil import *
+from ...Global import Global
+from ..RoleSelect import RoleSelect
+from anvil_extras import routing
 
 
 class PlanRow(PlanRowTemplate):
@@ -11,4 +14,8 @@ class PlanRow(PlanRowTemplate):
 
     def btn_show_roles_click(self, **event_args):
         """This method is called when the button is clicked"""
-        self.plan_role_perm = ''
+        self.plan_role_perm = self.item
+        self.plan_role_perm['roles'] = [i for i in Global.roles if i['name'] in self.item['roles']]
+        new_roles = routing.alert(RoleSelect(item=self.plan_role_perm))
+        if new_roles:
+            print(new_roles)
