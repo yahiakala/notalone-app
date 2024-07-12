dc6ce16c8513469e38799a7e743932c69d06872b
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-12 16:05:48 +0000

    Edited paypal

--- server_code/paypal.py
+++ server_code/paypal.py
@@ -10,7 +10,7 @@ from .emails import notify_paid
 
 
 # https://developer.paypal.com/docs/api/subscriptions/v1/#subscriptions_create
-if anvil.server.get_app_origin() and 'debug' in anvil.server.get_app_origin():
+if anvil.server.get_app_origin() is None or 'debug' in anvil.server.get_app_origin():
     # TOKEN_URL = 'https://api-m.sandbox.paypal.com/v1/oauth2/token'
     # SUBSCRIPTION_URL = 'https://api-m.sandbox.paypal.com/v1/billing/subscriptions'
     TOKEN_URL = 'https://api.paypal.com/v1/oauth2/token'
@@ -20,13 +20,10 @@ else:
     SUBSCRIPTION_URL = 'https://api.paypal.com/v1/billing/subscriptions'
 
 def get_paypal_auth(tenant, client_id=None, client_secret=None):
-    # TODO: accept client id and secret as args.
-    import base64
+    import json
+    
     client_id = client_id or anvil.secrets.decrypt_with_key('encryption_key', tenant['paypal_client_id'])
     client_secret = client_secret or anvil.secrets.decrypt_with_key('encryption_key', tenant['paypal_secret'])
-
-    client_id = base64.b64encode(client_id.encode('utf-8')).decode('utf-8')
-    client_secret = base64.b64encode(client_secret.encode('utf-8')).decode('utf-8')
     
     try:
         auth_response = anvil.http.request(
@@ -35,21 +32,18 @@ def get_paypal_auth(tenant, client_id=None, client_secret=None):
             username=client_id,
             password=client_secret,
             headers={
-                'Accept': 'application/json',
-                'Accept-Language': 'en_US',
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Accept': 'application/json'
             },
             data={'grant_type': 'client_credentials'},
-            json=True
         )
-        
-        # auth_response_json = auth_response.json()
+        # print(auth_response.get_bytes())
+        auth_response = json.loads(auth_response.get_bytes().decode('utf-8'))
         access_token = auth_response['access_token']
+        
         return access_token
     except anvil.http.HttpError as e:
-        print(client_id)
-        print(client_secret)
         print(f"Error {e.status} {e.content}")
+        print(e.content.get_bytes())
 
 
 def get_subscriptions(tenant, subscription_id, access_token=None, verbose=False):
