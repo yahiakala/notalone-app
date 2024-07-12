e7fdda44314e6728fdbd63c5850e544be95d2e0f
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-12 14:18:22 +0000

    Edited tests

--- server_code/tests.py
+++ server_code/tests.py
@@ -1,6 +1,16 @@
 """Test the functions that are not callables."""
+from anvil.tables import app_tables
+import anvil.users
+
 
 def create_test_data():
     # user who is on a volunteer-req payment tier who does not have a role.
     # Sample tenant
     pass
+
+
+@anvil.server.callable(require_user=True)
+def impersonate_user(email):
+    new_user = app_tables.users.get(email=email)
+    anvil.users.force_login(new_user)
+    return new_user
