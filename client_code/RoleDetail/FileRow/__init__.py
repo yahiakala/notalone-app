a82f1c1e1512ed23d275f49c3e05c3e2cba4807b
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-15 03:18:03 +0000

    Edited RoleDetail.FileRow and RoleDetail

--- client_code/RoleDetail/FileRow/__init__.py
+++ client_code/RoleDetail/FileRow/__init__.py
@@ -7,4 +7,15 @@ class FileRow(FileRowTemplate):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        # Any code you write here will run before the form opens.
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        if self.item:
+            self.lbl_name = self.item['name']
+            self.file = self.item['file']
+        else:
+            self.file = None
+
+    def btn_view_file_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        if self.file:
+            pass
