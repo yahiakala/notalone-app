9b88bd7e7d9d70c390f548bc9ac51ddedc8806a5
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-15 21:17:05 +0000

    Edited custom_reports

--- server_code/custom_reports.py
+++ server_code/custom_reports.py
@@ -1,5 +1,20 @@
 import anvil.server
+from anvil.tables import app_tables
+from .helpers import validate_user, usermap_row_to_dict
 
-def no_volunteer():
-    # look for people who ONLY have the 'Member' role.
-    pass
+
+@anvil.server.callable(require_user=True)
+def no_volunteer(tenant_id, tenant=None):
+    """look for people who ONLY have the 'Member' role and fee=10."""
+    user = anvil.users.get_user(allow_remembered=True)
+    tenant, usermap, permissions = validate_user(tenant_id, user)
+    if 'see_members' in permissions:
+        return None
+
+    member_role = app_tables.roles.get(tenant=tenant, name='Member')
+    usermaps = app_tables.usermap.search(roles=[member_role], fee=10)
+    user_list = []
+    for usermap in usermaps:
+        if len(usermap['roles']) == 1:
+            user_list.append(usermap_row_to_dict(usermap))
+    return user_list
