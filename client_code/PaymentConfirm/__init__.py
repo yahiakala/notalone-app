62c9769b72664cb6260203a86aa3b0183197f0f9
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-15 00:53:05 +0000

    Edited PaymentConfirm and payments

--- client_code/PaymentConfirm/__init__.py
+++ client_code/PaymentConfirm/__init__.py
@@ -17,14 +17,15 @@ class PaymentConfirm(PaymentConfirmTemplate):
     def ti_payment_tick(self, **event_args):
         """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
         self.email = Global.user['email']
-        self.member = anvil.server.call('get_member_data', Global.tenant_id, self.email)
-        print('ti_payment_tick')
-        if 'Member' in self.member['roles']:
-            self.ti_payment.interval = 0
-            Global.permissions = self.member['permissions']
-            print('Payment confirmed!')
-            self.label_1.visible = False
-            self.image_1.visible = False
-            self.lbl_confirmed.visible = True
-            routing.clear_cache()
+        with anvil.server.no_loading_indicator:
+            self.member = anvil.server.call('get_member_data', Global.tenant_id, self.email)
+            print('ti_payment_tick')
+            if 'Member' in self.member['roles']:
+                self.ti_payment.interval = 0
+                Global.permissions = self.member['permissions']
+                print('Payment confirmed!')
+                self.label_1.visible = False
+                self.image_1.visible = False
+                self.lbl_confirmed.visible = True
+                routing.clear_cache()
             
