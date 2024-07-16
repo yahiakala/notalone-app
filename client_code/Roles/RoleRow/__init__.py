92fb60daec85289c6f3ad574f20d98c8ea713cb7
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-16 13:57:48 +0000

    Edited Roles.RoleRow and Router

--- client_code/Roles/RoleRow/__init__.py
+++ client_code/Roles/RoleRow/__init__.py
@@ -1,15 +1,23 @@
 from ._anvil_designer import RoleRowTemplate
 from anvil import *
 from anvil_extras import routing
+from ...Global import Global
 
 
 class RoleRow(RoleRowTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        
 
-        # Any code you write here will run before the form opens.
-
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        if self.item:
+            self.lbl_name.text = self.item['name']
+            self.lbl_last_update.text = self.item['last_update']
+            self.lbl_name.role = None
+            self.lbl_last_update.role = None
+            
     def btn_edit_click(self, **event_args):
         """This method is called when the button is clicked"""
         routing.set_url_hash(url_pattern='app/roledetail', url_dict={'role': self.item['name']})
@@ -21,10 +29,4 @@ class RoleRow(RoleRowTemplate):
             url_dict={'role': self.item['name']}
         )
 
-    def form_show(self, **event_args):
-        """This method is called when the form is shown on the page"""
-        if self.item:
-            self.lbl_name.text = self.item['name']
-            self.lbl_last_update.text = self.item['last_update']
-            self.lbl_name.role = None
-            self.lbl_last_update.role = None
+
