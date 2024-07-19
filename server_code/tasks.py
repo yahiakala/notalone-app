3c9c9525a4e60d970995cc6db62b2f4d0d08e569
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-19 14:57:14 +0000

    Edited tasks and settings

--- server_code/tasks.py
+++ server_code/tasks.py
@@ -83,8 +83,10 @@ def edit_member(tenant_id, new_dict):
     if 'edit_members' in permissions:
         roles = app_tables.roles.search(tenant=tenant, name=q.any_of(*new_dict['roles']))
         membermap['roles'] = list(roles)
-        if len(usermap['roles']) == 0:
-            usermap['roles'] = None
+        # print_timestamp('len of roles')
+        # print(membermap['roles'])
+        if len(membermap['roles']) == 0:
+            membermap['roles'] = None
 
     if 'see_members' in permissions:
         membermap['notes'] = new_dict['notes']
