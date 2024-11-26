a84af9096f0dc03a1f1a832fc8e4240c0e144497
yahiakala <24422258+yahiakala@users.noreply.github.com>
2024-11-26 14:56:56 -0500

    add role to member screen if navigating from role screen

--- client_code/Members/__init__.py
+++ client_code/Members/__init__.py
@@ -1,16 +1,13 @@
-from ._anvil_designer import MembersTemplate
-from anvil import *
-import anvil.tables.query as q
 import anvil.server
+import anvil.tables.query as q
+from anvil import *
+from anvil_extras import routing
 
 from ..Global import Global
-from anvil_extras import routing
-# import datetime as dt
-# from anvil_extras.logging import TimerLogger
-# from anvil_squared.utils import print_timestamp
+from ._anvil_designer import MembersTemplate
 
 
-@routing.route('/members', template='Router', url_keys=[routing.ANY])
+@routing.route("/members", template="Router", url_keys=[routing.ANY])
 class Members(MembersTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
@@ -21,26 +18,32 @@ class Members(MembersTemplate):
         """This method is called when the form is shown on the page"""
         with anvil.server.no_loading_indicator:
             self.users = Global.users
-            
-            if 'role' in self.url_dict:
+
+            if "role" in self.url_dict:
                 self.start_search()
                 self.members = anvil.server.call(
-                    'search_users_by_role',
-                    Global.tenant_id,
-                    self.url_dict['role']
+                    "search_users_by_role", Global.tenant_id, self.url_dict["role"]
                 )
                 self.rp_members.items = self.members
                 self.pagination_1.data_grid = self.dg_members
                 self.pagination_1.repeating_panel = self.rp_members
+                # Show role name in label
+                self.lbl_role_filter.text = (
+                    f"Members with role: {self.url_dict['role']}"
+                )
+                self.lbl_role_filter.visible = True
                 self.end_search()
-            
+            else:
+                # Hide role label if no role filter
+                self.lbl_role_filter.visible = False
+
             if self.members == [None, None, None]:
                 self.members = Global.users.search(
                     q.fetch_only(
-                        'user', 'first_name', 'last_name',
-                        user=q.fetch_only(
-                            'email', 'last_login', 'signed_up'
-                        )
+                        "user",
+                        "first_name",
+                        "last_name",
+                        user=q.fetch_only("email", "last_login", "signed_up"),
                     )
                 )
             self.btn_qf_applicants.enabled = True
@@ -49,7 +52,7 @@ class Members(MembersTemplate):
             self.btn_qf_disabled.enabled = True
             self.btn_qf_inactive.enabled = True
             self.tb_mb_search.enabled = True
-    
+
             if self.members is not None:
                 self.rp_members.items = self.members
 
@@ -60,7 +63,9 @@ class Members(MembersTemplate):
         """This method is called when the user presses Enter in this text box"""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_text', Global.tenant_id, self.tb_mb_search.text)
+            self.members = anvil.server.call(
+                "search_users_by_text", Global.tenant_id, self.tb_mb_search.text
+            )
             self.end_search()
 
     def btn_clear_search_click(self, **event_args):
@@ -69,61 +74,73 @@ class Members(MembersTemplate):
         with anvil.server.no_loading_indicator:
             self.members = Global.users.search(
                 q.fetch_only(
-                    'user', 'first_name', 'last_name',
-                    user=q.fetch_only(
-                        'email', 'last_login', 'signed_up'
-                    )
+                    "user",
+                    "first_name",
+                    "last_name",
+                    user=q.fetch_only("email", "last_login", "signed_up"),
                 )
             )
             self.end_search()
             self.tb_mb_search.text = None
             self.btn_clear_search.enabled = False
+            # Hide role label when clearing search
+            self.lbl_role_filter.visible = False
 
     def btn_qf_admins_click(self, **event_args):
         """Get admins."""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_role', Global.tenant_id, 'Admin')
+            self.members = anvil.server.call(
+                "search_users_by_role", Global.tenant_id, "Admin"
+            )
             self.end_search()
 
     def btn_qf_applicants_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_role', Global.tenant_id, 'Applicant')
+            self.members = anvil.server.call(
+                "search_users_by_role", Global.tenant_id, "Applicant"
+            )
             self.end_search()
 
     def btn_qf_inactive_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_role', Global.tenant_id, 'Approved')
+            self.members = anvil.server.call(
+                "search_users_by_role", Global.tenant_id, "Approved"
+            )
             self.end_search()
-    
+
     def btn_qf_disabled_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_role', Global.tenant_id, None)
+            self.members = anvil.server.call(
+                "search_users_by_role", Global.tenant_id, None
+            )
             self.end_search()
 
     def btn_qf_regular_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.start_search()
         with anvil.server.no_loading_indicator:
-            self.members = anvil.server.call('search_users_by_role', Global.tenant_id, 'Member')
+            self.members = anvil.server.call(
+                "search_users_by_role", Global.tenant_id, "Member"
+            )
             self.end_search()
-    
+
     def reset_buttons(self, **event_args):
-        self.btn_qf_admins.role = 'tonal-button'
-        self.btn_qf_applicants.role = 'tonal-button'
-        self.btn_qf_disabled.role = 'tonal-button'
-        self.btn_qf_inactive.role = 'tonal-button'
-        self.btn_qf_regular.role = 'tonal-button'
+        self.btn_qf_admins.role = "tonal-button"
+        self.btn_qf_applicants.role = "tonal-button"
+        self.btn_qf_disabled.role = "tonal-button"
+        self.btn_qf_inactive.role = "tonal-button"
+        self.btn_qf_regular.role = "tonal-button"
 
     def start_search(self):
         self.reset_buttons()
-        self.btn_clear_search.text = 'Searching...'
+        self.btn_clear_search.text = "Searching..."
         self.btn_clear_search.italic = True
         self.btn_clear_search.enabled = True
         self.rp_members.items = [None, None, None]
@@ -131,7 +148,5 @@ class Members(MembersTemplate):
     def end_search(self):
         self.rp_members.items = self.members
         self.pagination_1.repeating_panel = self.rp_members
-        self.btn_clear_search.text = 'Clear Search'
+        self.btn_clear_search.text = "Clear Search"
         self.btn_clear_search.italic = False
-
-
